e3d4f2abe84ce54eb550d2ce032a0cb5cac392aa
Joe <55931862+Yekuhn@users.noreply.github.com>
2024-05-05 21:56:20 -0700

    Update data_prep.py

--- datasource/data_prep.py
+++ datasource/data_prep.py
@@ -6,13 +6,18 @@ import json
 
 processes = 4
 
+# Validation Dataset Sample Seed
 random.seed(21)
 
 uids = objaverse.load_uids()
-random_object_uids = random.sample(uids, 10)
+# Validation Dataset Size
+s_size = 5
+random_object_uids = random.sample(uids, s_size)
 
 
+# Validation Dataset Download Directory
 objaverse._VERSIONED_PATH = "/home/volt_zhou/val"
+
 valobjects = objaverse.load_objects(
     uids=random_object_uids,
     
@@ -21,12 +26,16 @@ valobjects = objaverse.load_objects(
     
 print(valobjects)
 
+# Training Dataset Sample Seed
 random.seed(42)
 
-random_object_uids = random.sample(uids, 30)
+# Training Dataset Sample Size
+s_size = 5
+random_object_uids = random.sample(uids, s_size)
 
-
+# Training Dataset Download Directory
 objaverse._VERSIONED_PATH = "/home/volt_zhou/train"
+
 trainobjects = objaverse.load_objects(
     uids=random_object_uids,
     
@@ -35,9 +44,3 @@ trainobjects = objaverse.load_objects(
     
 print(trainobjects)
 
-def decompress_json(input_path, output_path):
-    with gzip.open(input_path, 'rt') as gz_file, open(output_path, 'w') as json_file:
-        json.dump(json.load(gz_file), json_file, indent=4)
-        
-decompress_json('/home/volt_zhou/val/object-paths.json.gz', '/home/volt_zhou/val/object-paths.json')
-decompress_json('/home/volt_zhou/train/object-paths.json.gz', '/home/volt_zhou/train/object-paths.json')
